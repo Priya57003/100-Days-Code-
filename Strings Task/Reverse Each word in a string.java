28bc5ea4324ede6eb1cf6541a5214f6948ce71fd
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-06 09:21:39 -0800

    Update Reverse Each word in a string.java

--- Strings Task/Reverse Each word in a string.java
+++ Strings Task/Reverse Each word in a string.java
@@ -1,1 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		String str1[] = sc.nextLine().split(" ");
+		String rev="";
+		for(int i=0;i<str1.length;i++)
+		{
+		    String str2=str1[i];
+		    for(int j=str2.length()-1;j>=0;j--)
+		    {
+		         rev+=str2.charAt(j);
+		         
+		    }
+		   
+		    rev+=" ";
+		}
+		System.out.print(rev);
+		
+	
+	}
+}
 
