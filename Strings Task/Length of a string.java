fe455cc9ec4940b77f90355389895dd16862352a
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-06 09:00:41 -0800

    Create Length of a string.java

---
+++ Strings Task/Length of a string.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		String a=obj.nextLine();
+		System.out.println("Length of a string "+a.length());
+	}
+}
