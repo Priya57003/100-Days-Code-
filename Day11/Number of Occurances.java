26daaaaf13533c3b275408cf17fe6be86a6e1832
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-12 07:32:43 -0800

    Update Number of Occurances.java

--- Day11/Number of Occurances.java
+++ Day11/Number of Occurances.java
@@ -1,1 +1,38 @@
 
+import java.util.*;
+public class Main
+{
+    public static void findOccurance(int arr[][],int n1,int n2,int target)
+    {
+        int count=0;
+        for(int i=0;i<n1;i++)
+        {
+            for(int j=0;j<n2;j++)
+            {
+                if(arr[i][j]==target)
+                {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Number of Occurances of target"+count);
+        
+    }
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int arr[][]= new int[n1][n2];
+		for(int i=0;i<n1;i++)
+		{
+		   for(int j=0;j<n2;j++)
+		   {
+		       arr[i][j]=sc.nextInt();
+		   }
+		}
+		System.out.println("Enter element need to be found");
+		int target = sc.nextInt();
+		findOccurance(arr,n1,n2,target);
+	}
+}
