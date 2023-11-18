6dea9209f440b3db26d44f8d759460f49b5613fb
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-18 09:11:13 -0800

    Create Frequency of element using HashMap.java

---
+++ Day 14/Frequency of element using HashMap.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		HashMap<Integer,Integer> hs = new HashMap<>();
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    int num1 = sc.nextInt();
+		    if(!hs.containsKey(num1))
+		    {
+		        hs.put(num1,1);
+		    }
+		    else
+		    {
+		        hs.put(num1,hs.get(num1)+1);
+		    }
+		}
+		ArrayList<Integer> ar = new ArrayList<>(hs.keySet());
+		System.out.println("The frequency of number is equal to one ");
+		for(int num2:ar)
+		{
+		    if(hs.get(num2)==1)
+		    {
+		        
+		        System.out.print(num2+" ");
+		    }
+		}
+	}
+}
