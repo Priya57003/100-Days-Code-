cd71567bc7753816bb7ca0de12b74c447a7b6d21
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-23 03:55:36 -0800

    Create Stack opeartions.java

---
+++ Day 15/Stack opeartions.java
@@ -0,0 +1,15 @@
+
+	        if(i!=T)
+	        {
+	            if(st.isEmpty())
+	            {
+	                st.push(arr[i]); //1
+	            }
+	            else(st.peek()<arr[i]) //1<2<4
+	            {
+	                
+	                System.out.print(st.pop());
+	                
+	            }
+	            
+	        }
