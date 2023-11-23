42880408d733c7891a3c833eb7f0dd66cf8bfaf5
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-23 04:00:52 -0800

    Update Stack opeartions.java

--- Day 15/Stack opeartions.java
+++ Day 15/Stack opeartions.java
@@ -1,15 +1,27 @@
-
-	        if(i!=T)
-	        {
-	            if(st.isEmpty())
-	            {
-	                st.push(arr[i]); //1
-	            }
-	            else(st.peek()<arr[i]) //1<2<4
-	            {
-	                
-	                System.out.print(st.pop());
-	                
-	            }
-	            
-	        }
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	     int T = sc.nextInt();
+	    int arr[] = new int[n];
+	   
+	    Stack<Integer> st =new Stack<>();
+	    int c=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        arr[i] = sc.nextInt();
+	    }
+	    int min=Integer.MIN_VALUE;
+	    int element=0;
+	    for(int i=0;i<T;i++)
+	    {
+	        min=Math.max(min,arr[i]);
+	        st.push(min);
+	       
+	    }
+	     System.out.print(st.peek());
+	}
+}
