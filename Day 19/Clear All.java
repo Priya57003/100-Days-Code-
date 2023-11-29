4c4e18ea5bb7030ccf1119e8cf14a160eeae3d54
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-11-29 07:07:41 -0800

    Create Clear All.java

---
+++ Day 19/Clear All.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+      LinkedList < String > ll = new LinkedList <> ();
+      ll.add ("ABC");//0
+      ll.add ("def");//1
+      ll.add ("ghi");//2
+      ll.add("def");
+      ll.add("def");
+      ll.add ("ghi");
+      System.out.println("LinkedList1:");
+	  System.out.println(ll);
+      ll.clear();
+      System.out.println(ll);
+     
+       
+      
+      
+
+  }
+}
