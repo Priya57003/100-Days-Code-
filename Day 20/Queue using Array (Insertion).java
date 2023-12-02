9ad09e52766fb0202640a3b277f36b12de72ce7b
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-12-02 08:09:48 -0800

    Update Queue using Array (Insertion).java

--- Day 20/Queue using Array (Insertion).java
+++ Day 20/Queue using Array (Insertion).java
@@ -1,1 +1,42 @@
-
+import java.util.*;
+public class Main
+{
+    
+    int front,rear;
+    
+    Main()
+    {
+     front=-1;
+     rear=-1;
+    }
+    public  void enque(int queue[],int size,int element)
+    {
+        //No element is present in the queue so set to zero
+        if(front==-1)
+        {
+            front=0;
+        }
+        
+            ++rear;
+            queue[rear]=element;
+        
+        
+        System.out.print(Arrays.toString(queue));
+    }
+    
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		Main ob = new Main();
+		int size = sc.nextInt();
+		int queue[] = new int[size];
+		
+		for(int i=0;i<size;i++)
+		{
+		    int element=sc.nextInt();
+		ob.enque(queue,size,element);
+		}
+// 		displayQueue(queue,size);
+   
+	}
+}
