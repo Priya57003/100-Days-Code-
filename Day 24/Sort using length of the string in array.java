a997fc4ac488bb3e8f1a77d9b6bb40f7553e7f78
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-12-11 09:40:41 -0800

    Create Sort using length of the string in array.java

---
+++ Day 24/Sort using length of the string in array.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main implements Comparator
+{
+    // Main is not abstract and does not override abstract method compare(Object,Object) in Comparator
+    @Override
+    public int compare(Object o1,Object o2)
+    {
+        String s1 =(String)o1;
+        String s2 =(String)o2;
+        
+        if(s1.length()>s2.length())
+         return +1;
+         else if(s1.length()<s2.length())
+          return -1;
+          else
+          return 0;
+        //  positive -->asc unless desc
+    }
+	public static void main(String[] args) 
+	{
+		Main ob = new Main();
+// 		ob.compare("PriyaGowtham","AdityaAdithi");
+		
+         String arr[]={"sdfg","zasd","Aqwe","fgh","iuyt"};
+		Arrays.sort(arr,ob);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+}
