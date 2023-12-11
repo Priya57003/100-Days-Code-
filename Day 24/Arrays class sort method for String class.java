01fc7a9f1898fedbdaa471824cad5965e995bbaa
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-12-11 08:09:57 -0800

    Create Arrays class sort method for String class.java

---
+++ Day 24/Arrays class sort method for String class.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		String[] arr1={"zii","pow","out"};
+		String[] arr2={"Hello","uoyy"};
+		//Method overloading concepts used here that uses Arrays.equals(a,b)
+		Arrays.sort(arr1);
+		System.out.println(Arrays.toString(arr1));
+		//Defined classes are sort using sort method
+	}
+}
