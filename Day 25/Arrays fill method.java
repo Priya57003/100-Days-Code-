909128c62cf3418ad8f3d7194d4b518392d121c4
Priya57003 <96000441+Priya57003@users.noreply.github.com>
2023-12-12 08:03:41 -0800

    Create Arrays fill method.java

---
+++ Day 25/Arrays fill method.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int arr1[]={1,78,98};
+		Arrays.fill(arr1,10);
+		System.out.println(Arrays.toString(arr1));
+		
+	}
+}
